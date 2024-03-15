1495eeec195c8096d7ffcd04a7bb71d78c65c299
milesrichmond <miles.richmond24@d155.org>
2024-03-15 09:43:45 -0500

    stopwatch is functional, but just needs some more touches to assign to plans and things

--- Ultimate Race Planner/StopwatchViewController.swift
+++ Ultimate Race Planner/StopwatchViewController.swift
@@ -7,15 +7,18 @@
 
 import UIKit
 
-class StopwatchViewController: UIViewController {
+class StopwatchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var clock: UILabel!
     var clockValue: Double = 0.0 // Seconds
-    @IBOutlet weak var planSelect: UIPickerView!
+    var splits: [(TimeInterval, Float)] = []
+    @IBOutlet weak var splitTable: UITableView!
     
     var stopwatchTimer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        splitTable.delegate = self
+        splitTable.dataSource = self
     }
     
     func updateClock() {
@@ -23,6 +26,23 @@ class StopwatchViewController: UIViewController {
     }
 
     @IBAction func splitReset(_ sender: UIButton) {
+        if let timer = stopwatchTimer {
+            let currentTime = clockValue
+            splits.append(currentTime)
+            
+            splitTable.reloadData()
+            
+            sender.setTitle("Lap", for: .normal)
+            sender.setTitleColor(.systemGray, for: .normal)
+            sender.setTitleColor(.systemGray, for: .selected)
+        } else {
+            clockValue = 0.0
+            splits.removeAll()
+            clock.text = getFormattedTime(0)
+            
+            splitTable.reloadData()
+        }
+        
         
     }
     
@@ -30,14 +50,19 @@ class StopwatchViewController: UIViewController {
         if let timer = stopwatchTimer {
             timer.invalidate()
             stopwatchTimer = nil
-            sender.titleLabel?.text = "Start"
+            sender.setTitle("Start", for: .normal)
+            sender.setTitleColor(.systemGreen, for: .normal)
+            sender.setTitleColor(.systemGreen, for: .selected)
         } else {
-            clockValue = 0.0
             stopwatchTimer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { (timer) in
                 self.clockValue += timer.timeInterval
                 
                 self.clock.text = self.getFormattedTime(self.clockValue)
             })
+            
+            sender.setTitle("Stop", for: .normal)
+            sender.setTitleColor(.systemRed, for: .normal)
+            sender.setTitleColor(.systemRed, for: .selected)
         }
     }
     
@@ -48,4 +73,18 @@ class StopwatchViewController: UIViewController {
         
         return "\(minutes):\(seconds).\(milliseconds)"
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return splits.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "split", for: indexPath) as! SplitCell
+        
+        cell.time.text = "\(getFormattedTime(splits[indexPath.row]))"
+        cell.distanceRange.text = ""
+        
+        return cell
+    }
+    
 }
