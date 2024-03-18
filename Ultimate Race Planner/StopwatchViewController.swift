f78b5e8fbc0359a6f0438344c760a2633feb6d37
milesrichmond <miles.richmond24@d155.org>
2024-03-18 10:04:15 -0500

    doin a thing

--- Ultimate Race Planner/StopwatchViewController.swift
+++ Ultimate Race Planner/StopwatchViewController.swift
@@ -7,10 +7,14 @@
 
 import UIKit
 
-class StopwatchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class StopwatchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIPickerViewDelegate, UIPickerViewDataSource {
     @IBOutlet weak var clock: UILabel!
     var clockValue: Double = 0.0 // Seconds
     var splits: [(TimeInterval, Float)] = []
+    var selectedPlanIndex: Int = 0
+    
+    @IBOutlet weak var resetLapButton: UIButton!
+    @IBOutlet weak var planSelect: UIPickerView!
     @IBOutlet weak var splitTable: UITableView!
     
     var stopwatchTimer: Timer?
@@ -19,6 +23,8 @@ class StopwatchViewController: UIViewController, UITableViewDelegate, UITableVie
         super.viewDidLoad()
         splitTable.delegate = self
         splitTable.dataSource = self
+        planSelect.delegate = self
+        planSelect.dataSource = self
     }
     
     func updateClock() {
@@ -26,9 +32,9 @@ class StopwatchViewController: UIViewController, UITableViewDelegate, UITableVie
     }
 
     @IBAction func splitReset(_ sender: UIButton) {
-        if let timer = stopwatchTimer {
+        if stopwatchTimer != nil {
             let currentTime = clockValue
-            splits.append(currentTime)
+            splits.append((currentTime, Float.nan))
             
             splitTable.reloadData()
             
@@ -42,8 +48,6 @@ class StopwatchViewController: UIViewController, UITableViewDelegate, UITableVie
             
             splitTable.reloadData()
         }
-        
-        
     }
     
     @IBAction func startStop(_ sender: UIButton) {
@@ -53,6 +57,10 @@ class StopwatchViewController: UIViewController, UITableViewDelegate, UITableVie
             sender.setTitle("Start", for: .normal)
             sender.setTitleColor(.systemGreen, for: .normal)
             sender.setTitleColor(.systemGreen, for: .selected)
+            
+            resetLapButton.setTitle("Reset", for: .normal)
+            resetLapButton.setTitleColor(.systemRed, for: .normal)
+            resetLapButton.setTitleColor(.systemRed, for: .selected)
         } else {
             stopwatchTimer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { (timer) in
                 self.clockValue += timer.timeInterval
@@ -63,6 +71,10 @@ class StopwatchViewController: UIViewController, UITableViewDelegate, UITableVie
             sender.setTitle("Stop", for: .normal)
             sender.setTitleColor(.systemRed, for: .normal)
             sender.setTitleColor(.systemRed, for: .selected)
+            
+            resetLapButton.setTitle("Lap", for: .normal)
+            resetLapButton.setTitleColor(.systemGray, for: .normal)
+            resetLapButton.setTitleColor(.systemGray, for: .selected)
         }
     }
     
@@ -74,6 +86,16 @@ class StopwatchViewController: UIViewController, UITableViewDelegate, UITableVie
         return "\(minutes):\(seconds).\(milliseconds)"
     }
     
+    
+    
+    //
+    //
+    //
+    // ---------------------------------------------------------------------------------------------
+    //
+    //
+    //
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return splits.count
     }
@@ -81,10 +103,28 @@ class StopwatchViewController: UIViewController, UITableViewDelegate, UITableVie
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "split", for: indexPath) as! SplitCell
         
-        cell.time.text = "\(getFormattedTime(splits[indexPath.row]))"
-        cell.distanceRange.text = ""
+        cell.time.text = "\(getFormattedTime(splits[indexPath.row].0))"
+        cell.distanceRange.text = "\(splits[indexPath.row].1)"
+        let difference = splits[indexPath.row].1
+        cell.difference.text = "\(difference)"
         
         return cell
     }
     
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return AppData.plans.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return AppData.plans[row].name
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedPlanIndex = row
+        splitTable.reloadData()
+    }
 }
